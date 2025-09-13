ee731a0635ffbd82c06175e6cad7134fc5f44fc4
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2025-09-13 09:22:57 -0300

    desativando o dark mode 3

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,43 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import PrimeVue from 'primevue/config';
-import Button from 'primevue/button';
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
 
-import './style.css'; // Tailwind
-import 'leaflet/dist/leaflet.css';
+import "./style.css"; // Tailwind
+import "leaflet/dist/leaflet.css";
 
-import 'primeicons/primeicons.css'; // Ícones PrimeVue
+import "primeicons/primeicons.css"; // Ícones PrimeVue
 
-import Lara from '@primeuix/themes/lara';
-import router from './router/index.js';
-import { Toast, ToastService } from 'primevue';
-import Chart from 'primevue/chart';
-import 'chart.js/auto'
-import { createPinia } from 'pinia';
+import Lara from "@primeuix/themes/lara";
+import router from "./router/index.js";
+import { Toast, ToastService } from "primevue";
+import Chart from "primevue/chart";
+import "chart.js/auto";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
 
 // cria a instância do pinia
-const pinia = createPinia()
+const pinia = createPinia();
 
 // registra no app
-app.use(pinia)
+app.use(pinia);
 
 app.use(PrimeVue, {
-    theme: {
-        preset: Lara,
-        darkModeSelector: 'none',
-    }
+  theme: {
+    preset: Lara,
+    options: {
+      darkModeSelector: "none",
+    },
+  },
 });
 
-app.use(ToastService)
+app.use(ToastService);
 
-app.component('Button', Button);
-app.component('Toast', Toast);
-app.component('Chart', Chart)
+app.component("Button", Button);
+app.component("Toast", Toast);
+app.component("Chart", Chart);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app');
+app.mount("#app");
